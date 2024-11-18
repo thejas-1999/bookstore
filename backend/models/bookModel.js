f6547dc8e17877f6c66dfe5d1d01ead278892f1d
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2024-11-18 11:28:48 +0530

    create a book model

---
+++ backend/models/bookModel.js
@@ -0,0 +1,27 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const bookschema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    publishedYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Book = mongoose.model("Book", bookschema);
