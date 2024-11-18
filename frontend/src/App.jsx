1a8335873837616afaef603d8dd81b83ba3a9358
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2024-11-18 15:28:29 +0530

    create basic pages and add router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ShowBook from "./pages/ShowBook";
+import EditBook from "./pages/EditBook";
+import CreateBook from "./pages/CreateBook";
+import DeleteBook from "./pages/DeleteBook";
 
 function App() {
   return (
     <>
-      <h1 className="text-7xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books/create" element={<CreateBook />} />
+        <Route path="/books/details/:id" element={<ShowBook />} />
+        <Route path="/books/edit/:id" element={<EditBook />} />
+        <Route path="/books/delete/:id" element={<DeleteBook />} />
+      </Routes>
     </>
   );
 }
