6066f021cbb715500a78f71a04752732ddf83f5b
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2024-11-18 15:03:48 +0530

    initialize the tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,9 @@
 import "./App.css";
-import { useEffect } from "react";
-import axios from "axios";
 
 function App() {
-  useEffect(() => {
-    axios
-      .get("http://localhost:5555/api/books")
-      .then((response) => console.log(response));
-  }, []);
   return (
     <>
-      <h1>Simple program to test cors</h1>
+      <h1 className="text-7xl font-bold underline">Hello world!</h1>
     </>
   );
 }
