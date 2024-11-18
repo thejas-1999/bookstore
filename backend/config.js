5062900936e9a873ab145e41751566b13b4f6f8a
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2024-11-18 11:17:26 +0530

    create a monoose server and add

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const MONGODBURI =
+  "mongodb+srv://thejask074:feNegj3ikxY6ZA6V@bookstore.5k7p0.mongodb.net/?retryWrites=true&w=majority&appName=bookstore";
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import { PORT, MONGODBURI } from "./config.js";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`app is running on http://localhost:${PORT}`);
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(200).send("welcome");
 });
+
+mongoose
+  .connect(MONGODBURI)
+  .then(() => {
+    console.log("Database connected successfully");
+    app.listen(PORT, () => {
+      console.log(`app is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
